2f581d442fcbe59eeff4c66b4786e2dfbc032844
Anna Strelchenko <ipz193_sat@student.ztu.edu.ua>
2021-03-23 16:03:03 +0200

    lab5

--- frontend/views/hierarchy/index.php
+++ frontend/views/hierarchy/index.php
@@ -1,9 +1,9 @@
 <h2>Categories list:</h2>
+
 <?php
 function drawMenu ($listOfItems, $model) {
     echo "<ul>";
     foreach ($listOfItems as $item) {
-
         echo "<li>" . $item['name'];
         if ($model->hasChildren($item['id'])) {
             drawMenu(($model->getChildren($item['id'])), $model);
